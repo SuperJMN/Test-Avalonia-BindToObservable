5a795d1de6d786e94c572d6ff079cf6e6b053182
José Manuel Nieto <superjmn@outlook.com>
2022-08-02 01:05:38 +0200

    Doesn't work

---
+++ BindToObservable/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Linq;
+using DynamicData;
+using ReactiveUI;
+
+namespace BindToObservable.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    private readonly ReadOnlyObservableCollection<MyViewModel> myCollection;
+
+    public MainWindowViewModel()
+    {
+        var changes = Observable.Range(1, 4)
+            .Select(i => new MyViewModel(i))
+            .ToObservableChangeSet()
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Publish();
+
+        changes
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Bind(out myCollection)
+            .Subscribe();
+
+        IsSomethingSelected = changes
+            .AutoRefresh(x => x.IsSelected)
+            .ToCollection()
+            .Select(x => x.Any(model => model.IsSelected));
+
+        IsSomethingSelected.Subscribe(v => Debug.WriteLine($"Something selected: {v}"));
+
+        changes.Connect();
+    }
+
+    public IObservable<bool> IsSomethingSelected { get; }
+
+    public ReadOnlyObservableCollection<MyViewModel> MyCollection => myCollection;
+}
+
+public class MyViewModel : ReactiveObject
+{
+    private bool isSelected;
+
+    public MyViewModel(int i)
+    {
+        Number = i;
+    }
+
+    public int Number { get; }
+
+    public bool IsSelected
+    {
+        get => isSelected;
+        set => this.RaiseAndSetIfChanged(ref isSelected, value);
+    }
+}
---
+++ BindToObservable/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BindToObservable.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
